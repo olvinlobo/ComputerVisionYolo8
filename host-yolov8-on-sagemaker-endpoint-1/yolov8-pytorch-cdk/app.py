e5a709abf7a1a6e68e1c34c8a12c7e13c1bd4bba
olvinlobo <56222526+olvinlobo@users.noreply.github.com>
2024-09-29 10:57:34 -0500

    Delete host-yolov8-on-sagemaker-endpoint-1 directory

--- host-yolov8-on-sagemaker-endpoint-1/yolov8-pytorch-cdk/app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-import aws_cdk as cdk
-from cdk.yolov8_sagemaker import YOLOv8SageMakerStack
-
-app = cdk.App()
-YOLOv8SageMakerStack(app, "YOLOv8SageMakerStack",)
-
-app.synth()
